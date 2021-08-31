906579bb594e210a4a98b793d3e264dced9fb1e9
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2021-08-30 21:38:59 -0400

    learning some variable types and imports for Dates

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Date;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    // Primitive Data Types for storing simple data  like strings or numbers and reference types like objects
+    int number = 100;
+    double myDouble = 100.01;
+    String brand = "RandomSavage";
+    Date date = new Date();
+
+    System.out.println("Hello World" + date);
   }
 }
